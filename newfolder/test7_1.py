800f3b1815529ec3f9728afb8745668f19dc22d9
Yinglun Song <167911216+sycamoreeee@users.noreply.github.com>
2026-01-17 00:08:17 -0500

    delet files

--- newfolder/test7_1.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("FinTech-545-Fall2025/testfiles/data/test7_1.csv")
-
-mu = df["x1"].mean()
-sigma = df["x1"].std()
-
-print("mu:", mu)
-print("sigma:", sigma)
